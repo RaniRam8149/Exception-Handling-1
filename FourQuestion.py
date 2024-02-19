318bd133cef733ce405d7ed5e5da7c6a200b07d2
RaniRam8149 <156404927+RaniRam8149@users.noreply.github.com>
2024-02-19 15:30:33 +0530

    Update FourQuestion.py

--- FourQuestion.py
+++ FourQuestion.py
@@ -11,3 +11,38 @@ except Exception as e:
 else:
     f.close()
     print("this will be exxecuted once your try will execute without error")
+    
+b. Finally
+In this example, the finally block contains code that will always run, regardless of whether an exception occurs or not.
+def divide_numbers(a, b):
+    try:
+        result = a / b
+        print("Division successful. Result:", result)
+    except ZeroDivisionError:
+        print("Error: Division by zero is not allowed.")
+    finally:
+        print("This block always runs, regardless of exceptions.")
+
+divide_numbers(10, 2)
+
+divide_numbers(10, 0)
+
+c. raise
+In this example, the raise statement is used to deliberately raise a custom exception.
+class validataege(Exception):
+    def __init__(self,msg):
+        self.msg=msg
+
+def validaetage(age):
+    if age<0:
+        raise validateage("entered age is negative")
+    elif age>200:
+        raise validateage("entered age is very high")
+    else:
+        print("age is valid")
+
+try:
+    age=int(input("enter your age"))
+    validaetage(age)
+except validaetage as e:
+    print(e)
